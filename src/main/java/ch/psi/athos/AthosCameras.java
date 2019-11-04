d1a0f897c16ee4b8a9a89f7190369b3a438e8981
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-11-04 16:13:51 +0100

    Channel plots

--- src/main/java/ch/psi/athos/AthosCameras.java
+++ src/main/java/ch/psi/athos/AthosCameras.java
@@ -5,26 +5,37 @@ import ch.psi.pshell.bs.StreamValue;
 import ch.psi.pshell.core.CommandSource;
 import ch.psi.pshell.device.Device;
 import ch.psi.pshell.device.DeviceAdapter;
+import ch.psi.pshell.device.DeviceListener;
+import ch.psi.pshell.device.Readable.ReadableArray;
+import ch.psi.pshell.device.Readable.ReadableNumber;
+import ch.psi.pshell.device.ReadableRegister.ReadableRegisterArray;
+import ch.psi.pshell.device.ReadableRegister.ReadableRegisterNumber;
 import ch.psi.pshell.imaging.Overlay;
 import ch.psi.pshell.scan.MonitorScan;
 import ch.psi.pshell.swing.DataPanel;
+import ch.psi.pshell.swing.DeviceValueChart;
 import ch.psi.pshell.ui.App;
 import ch.psi.pshell.ui.Panel;
 import ch.psi.utils.Arr;
+import ch.psi.utils.Convert;
 import ch.psi.utils.State;
+import ch.psi.utils.Str;
 import ch.psi.utils.Threading;
 import ch.psi.utils.swing.MainFrame;
 import ch.psi.utils.swing.SwingUtils;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
@@ -47,12 +58,16 @@ public class AthosCameras extends Panel {
     static String pipelineSuffixImage = "_aci";
     static double imageFrameRate = 2.1;
     
-    boolean dataChanged;
-    
+    boolean dataChanged;    
     boolean persisting;
+    
+    final DefaultTableModel model;
+    
+    Map<String, JDialog> deviceDialogs = new HashMap<>();
     
     public AthosCameras() {
         initComponents();
+        model = (DefaultTableModel) table.getModel();
         labelRecording.setVisible(false);
         buttonOpen.setEnabled(false);
         viewer.setPipelineNameFormat("%s" + pipelineSuffixImage);
@@ -118,7 +133,7 @@ public class AthosCameras extends Panel {
             config.put("camera_name", cameraName);
             config.put("include", fields.toArray(new String[0]));
             dataPipeline.setInstanceConfig(config);
-            dataPipeline.savePipelineConfig(cameraName, config);        
+            dataPipeline.savePipelineConfig(getDataPipeline(), config);        
             dataChanged = true;
         }
     }
@@ -129,7 +144,8 @@ public class AthosCameras extends Panel {
         
         boolean changed = !String.valueOf(cameraName).equals(this.cameraName);
         this.cameraName = cameraName;
-
+        model.setRowCount(0);
+        updateButtons();
 
         textCamera.setText((cameraName == null) ? "" : cameraName);
         if (cameraName == null) {
@@ -184,8 +200,7 @@ public class AthosCameras extends Panel {
         return String.format("%1.4f", sv.getValue(id));
     }
     
-    void updateData(StreamValue value){        
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
+    void updateData(StreamValue value){                
         List<String> ids = value.getIdentifiers();
         List values = value.getValues();
         if (dataChanged){
@@ -213,7 +228,7 @@ public class AthosCameras extends Panel {
         System.out.println("startRecording");
         getContext().startExecution(CommandSource.plugin, null, cameraName,null, false);
         getContext().setExecutionPar("name", cameraName);
-        getContext().setExecutionPar("layout", "default");
+        //getContext().setExecutionPar("layout", "default");
         getContext().setExecutionPar("open", true);
         scan=  new MonitorScan(dataPipeline.getStream(), dataPipeline.getStream().getReadables().toArray(new ch.psi.pshell.device.Readable[0]), -1, -1);
         Threading.getFuture(() ->scan.start());          
@@ -230,10 +245,76 @@ public class AthosCameras extends Panel {
         getContext().endExecution();
     }
     
+
     void openFile() throws Exception{
         String filename = textFile.getText();
         DataPanel panel = DataPanel.create(new File(filename));
         SwingUtils.showDialog(getTopLevel(), filename, new Dimension(600,400), panel);        
+    }
+    
+    void showPlot(String field) throws Exception{        
+        String title = cameraName + " " + field;
+        if (deviceDialogs.containsKey(title)){
+            JDialog dlg =  deviceDialogs.get(title);
+            if ((dlg!=null) && dlg.isShowing()){
+                dlg.requestFocus();
+                return;
+            }
+        }
+        DeviceListener listener;
+        Object obj = dataPipeline.getValue(field);        
+        if (field.equals("processing_parameters")) {
+            Map<String, Object> pars = viewer.getProcessingParameters(dataPipeline.getStream().take());
+            StringBuilder sb = new StringBuilder();
+            for (String key : pars.keySet()) {
+                sb.append(key).append(" = ").append(Str.toString(pars.get(key), 10)).append("\n");
+            }
+            SwingUtils.showMessage(this, "Processing Parameters", sb.toString());
+        } else if ((obj != null) && (obj.getClass().isArray() || (obj instanceof Number))) {
+            DeviceValueChart chart = new DeviceValueChart();
+            chart.setAsyncUpdates(true);
+                        
+            Device dev = null;
+            if (obj.getClass().isArray()) {
+                dev = new ReadableRegisterArray(new ReadableArray() {
+                    @Override
+                    public Object read() throws IOException, InterruptedException {
+                        return Convert.toDouble(dataPipeline.getValue(field));
+                    }
+
+                    @Override
+                    public int getSize() {
+                        return Array.getLength(dataPipeline.getValue(field));
+                    }
+                });
+            } else {
+                dev = new ReadableRegisterNumber(new ReadableNumber() {
+                    @Override
+                    public Object read() throws IOException, InterruptedException {
+                        return Convert.toDouble(dataPipeline.getValue(field));
+                    }
+                });
+            }
+            //dev.setPolling(1000);
+            Device finalDev = dev;
+            chart.setDevice(dev); 
+            
+            listener = new DeviceAdapter() {
+                @Override
+                public void onCacheChanged(Device device, Object value, Object former, long timestamp, boolean valueChange) {
+                    finalDev.updateAsync();
+                }
+            };
+            dataPipeline.getStream().addListener(listener);
+            JDialog dlg = SwingUtils.showDialog(AthosCameras.this.getTopLevel(), title, null, chart);
+
+            deviceDialogs.put(title, dlg);
+        }         
+    }
+    
+    void updateButtons(){
+        buttonPlot.setEnabled((table.getRowCount()>0) && (table.getSelectedRowCount()==1));
+        buttonSelect.setEnabled(cameraName!=null);
     }
     
     
@@ -266,6 +347,7 @@ public class AthosCameras extends Panel {
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
         buttonSelect = new javax.swing.JButton();
+        buttonPlot = new javax.swing.JButton();
         jPanel4 = new javax.swing.JPanel();
         textCamera = new javax.swing.JTextField();
         viewer = new ch.psi.pshell.bs.StreamCameraViewer();
@@ -383,12 +465,31 @@ public class AthosCameras extends Panel {
                 return canEdit [columnIndex];
             }
         });
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                tableMouseReleased(evt);
+            }
+        });
+        table.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                tableKeyReleased(evt);
+            }
+        });
         jScrollPane1.setViewportView(table);
 
-        buttonSelect.setText("Select");
+        buttonSelect.setText("Select Fields");
+        buttonSelect.setEnabled(false);
         buttonSelect.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonSelectActionPerformed(evt);
+            }
+        });
+
+        buttonPlot.setText("Plot");
+        buttonPlot.setEnabled(false);
+        buttonPlot.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonPlotActionPerformed(evt);
             }
         });
 
@@ -397,21 +498,28 @@ public class AthosCameras extends Panel {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(buttonPlot)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(buttonSelect)))
                 .addContainerGap())
         );
+
+        jPanel2Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {buttonPlot, buttonSelect});
+
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(buttonSelect)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonSelect)
+                    .addComponent(buttonPlot))
                 .addContainerGap())
         );
 
@@ -512,8 +620,28 @@ public class AthosCameras extends Panel {
         }
     }//GEN-LAST:event_buttonSelectActionPerformed
 
+    private void buttonPlotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonPlotActionPerformed
+        try{
+            if (table.getSelectedRow()>=0){
+                String field = (String) model.getValueAt(table.getSelectedRow(), 0);
+                showPlot(field);
+            }
+        } catch (Exception ex){
+            this.showException(ex);
+        }
+    }//GEN-LAST:event_buttonPlotActionPerformed
+
+    private void tableMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableMouseReleased
+        updateButtons();
+    }//GEN-LAST:event_tableMouseReleased
+
+    private void tableKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tableKeyReleased
+        updateButtons();
+    }//GEN-LAST:event_tableKeyReleased
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonOpen;
+    private javax.swing.JButton buttonPlot;
     private javax.swing.JToggleButton buttonRec;
     private javax.swing.JButton buttonSelect;
     private javax.swing.JButton buttonStop;
